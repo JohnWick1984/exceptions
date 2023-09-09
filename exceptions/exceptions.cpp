b20715dffe1985a0fd26515f71ce9b4cc72177ac
Евгений <@EUGENE1984>
2023-09-09 17:25:39 +0500

    Добавьте файлы проекта.

---
+++ exceptions/exceptions.cpp
@@ -0,0 +1,79 @@
+﻿// exceptions.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+class QueuePriority
+{
+private:
+    int queue[MAX_SIZE];
+    int priorities[MAX_SIZE];
+    int size;
+
+public:
+    QueuePriority() : size(0) {}
+
+    void insert(int value, int priority)
+    {
+        if (size >= MAX_SIZE)
+        {
+            throw "Queue is overflow!"; // Бросаем строку как исключение
+        }
+
+        int index = size;
+        while (index > 0 && priority < priorities[index - 1])
+        {
+            queue[index] = queue[index - 1];
+            priorities[index] = priorities[index - 1];
+            index--;
+        }
+
+        queue[index] = value;
+        priorities[index] = priority;
+        size++;
+    }
+
+    int extract()
+    {
+        if (size == 0)
+        {
+            throw "Queue is empty!"; // Бросаем строку как исключение
+        }
+
+        int value = queue[0];
+
+        for (int i = 0; i < size - 1; i++)
+        {
+            queue[i] = queue[i + 1];
+            priorities[i] = priorities[i + 1];
+        }
+
+        size--;
+        return value;
+    }
+};
+
+int main()
+{
+    QueuePriority queue;
+
+    try
+    {
+        queue.insert(10, 2);
+        queue.insert(20, 1);
+        queue.insert(30, 3);
+
+        cout << "Extracting from queue: " << queue.extract() << endl;
+        cout << "Extracting from queue: " << queue.extract() << endl;
+        cout << "Extracting from queue: " << queue.extract() << endl;
+        cout << "Extracting from queue: " << queue.extract() << endl;
+    }
+    catch (const char* e)
+    {
+        cout << "Exception: " << e << endl;
+    }
+
+    return 0;
+}
---
+++ namespace/namespace.cpp
@@ -0,0 +1,102 @@
+﻿// namespace.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+// Пространство имён для вычислений на координатной плоскости
+namespace CoordinateSpace {
+    // Класс для точки на плоскости
+    class Point 
+    {
+    public:
+        Point() : x(0.0), y(0.0) {} 
+
+        Point(double xCoord, double yCoord) : x(xCoord), y(yCoord) {}
+
+        double getX() const { return x; }
+        double getY() const { return y; }
+
+    private:
+        double x;
+        double y;
+    };
+
+    // Класс для многоугольника
+    class Polygon 
+    {
+    public:
+        Polygon() : numVertices(0), vertices(nullptr) {}
+        Polygon(int numVerticesCount) : numVertices(numVerticesCount), vertices(new Point[numVerticesCount]) {}
+
+        void setVertex(int index, double xCoord, double yCoord) 
+        {
+            if (index >= 0 && index < numVertices) 
+            {
+                vertices[index] = Point(xCoord, yCoord);
+            }
+        }
+
+        const Point* getVertices() const { return vertices; }
+        int getNumVertices() const { return numVertices; }
+
+    private:
+        int numVertices;
+        Point* vertices;
+    };
+}
+
+// Пространство имён для классов-хранилищ
+namespace StorageSpace {
+    // Класс для динамического массива точек
+    class DynamicArray 
+    {
+    public:
+        DynamicArray(int capacityCount) : capacity(capacityCount), size(0), points(new CoordinateSpace::Point[capacityCount]) {}
+
+        void addPoint(double xCoord, double yCoord) 
+        {
+            if (size < capacity)
+            {
+                points[size] = CoordinateSpace::Point(xCoord, yCoord);
+                size++;
+            }
+        }
+
+        const CoordinateSpace::Point* getPoints() const { return points; }
+        int getSize() const { return size; }
+
+    private:
+        int capacity;
+        int size;
+        CoordinateSpace::Point* points;
+    };
+}
+
+int main()
+{
+    using namespace CoordinateSpace;
+    using namespace StorageSpace;
+
+    // Создаем динамический массив точек
+    DynamicArray pointArray(3);
+    pointArray.addPoint(1.0, 2.0);
+    pointArray.addPoint(3.0, 4.0);
+    pointArray.addPoint(5.0, 6.0);
+
+    // Преобразуем массив точек в многоугольник
+    Polygon polygon(pointArray.getSize());
+    const Point* points = pointArray.getPoints();
+    for (int i = 0; i < pointArray.getSize(); i++) 
+    {
+        polygon.setVertex(i, points[i].getX(), points[i].getY());
+    }
+
+    // Выводим координаты вершин многоугольника
+    const Point* vertices = polygon.getVertices();
+    for (int i = 0; i < polygon.getNumVertices(); i++)
+    {
+        cout << "X: " << vertices[i].getX() << ", Y: " << vertices[i].getY() << endl;
+    }
+
+    return 0;
+}
